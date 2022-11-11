dc6eb62c44715d4bf16d3b81adc019679bb962a8
Michael Belair <belair.34@osu.edu>
2022-11-11 13:41:37 -0500

    REVIVED ZELDA GAME

---
+++ ZELDA/EnemyStates/EStateAnimatedKeese.cs
@@ -0,0 +1,70 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Game1.Projectiles;
+
+namespace Game1.PlayerStates
+{
+    class EStateAnimatedKeese : IEnemyState
+    {
+        ISprite sprite;
+        IEnemy enemy;
+        int coolDown;
+        public EStateAnimatedKeese(IEnemy enemy, int coolDown = 0)
+        {
+            this.enemy = enemy;
+            this.sprite = null;
+            this.coolDown = coolDown;
+        }
+        public void MoveUp()
+        {
+            //enemy.SetState(new EStateMovingUp(enemy, coolDown));
+        }
+
+        public void MoveDown()
+        {
+            //enemy.SetState(new EStateMovingDown(enemy, coolDown));
+        }
+        public void MoveLeft()
+        {
+            //enemy.SetState(new EStateMovingLeft(enemy, coolDown));
+        }
+
+        public void MoveRight()
+        {
+            //enemy.SetState(new EStateMovingRight(enemy, coolDown));
+        }
+        public void MoveToPlayer()
+        {
+            //enemy.SetState(new EStateMovingToPlayer(enemy, coolDown));
+        }
+        public void MoveHorizontal()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MoveVertical()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void Stop()
+        {
+
+        }
+
+        public void Update()
+        {
+            if (coolDown > 0)
+            {
+                coolDown--;
+            }
+            sprite.Update();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.Draw(spriteBatch);
+        }
+
+    }
+}
